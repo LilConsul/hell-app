4f2ea38221083c04e8a5689cdceff19f66613e3c
Denys Shevchenko <denshevchenko19@gmail.com>
2025-03-27 10:42:32 +0100

    Update frontend Dockerfile

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {BrowserRouter as Router, Route, Routes} from 'react-router';
+
 import './index.css'
 import App from './App.jsx'
 

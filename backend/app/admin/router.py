25d55a0341a21e531407aea0bd742f97a0c0a955
Denys Shevchenko <denshevchenko19@gmail.com>
2025-04-27 14:59:28 +0200

    Add admin service and user management endpoints

--- backend/app/admin/router.py
+++ backend/app/admin/router.py
@@ -1,17 +1,80 @@
+from typing import List
+
 from fastapi import APIRouter
 from fastapi.params import Depends
 
+from app.admin.dependencies import get_admin_service
+from app.admin.service import AdminService
 from app.auth.dependencies import get_current_admin_id
+from app.auth.schemas import UserRole
+from app.core.schemas import BaseReturn
+from app.users.schemas import UserSchema
 
 router = APIRouter(
-    prefix="/admin",
-    tags=["admin"],
-    dependencies=[Depends(get_current_admin_id)]
+    prefix="/admin", tags=["admin"], dependencies=[Depends(get_current_admin_id)]
 )
 
-@router.get("/users",)
-async def get_users():
+
+@router.get(
+    "/users",
+    response_model=BaseReturn[List[UserSchema]],
+    response_model_exclude_none=True,
+)
+async def get_users(
+    admin_id: str = Depends(get_current_admin_id),
+    admin_service: AdminService = Depends(get_admin_service),
+):
     """
     Get all users
     """
-    return {"message": "Get all users"}
+    users = await admin_service.get_all_users(admin_id)
+    return BaseReturn(message="Users retrieved successfully", data=users)
+
+
+@router.post(
+    "/users/change-role",
+    response_model=BaseReturn[UserSchema],
+    response_model_exclude_none=True,
+)
+async def change_user_role(
+    user_id: str,
+    role: UserRole,
+    admin_service: AdminService = Depends(get_admin_service),
+):
+    """
+    Change user role
+    """
+    user = await admin_service.change_user_role(user_id, role)
+    return BaseReturn(message="User role changed successfully", data=user)
+
+
+@router.delete(
+    "/users/{user_id}",
+    response_model=BaseReturn[UserSchema],
+    response_model_exclude_none=True,
+)
+async def delete_user(
+    user_id: str,
+    admin_service: AdminService = Depends(get_admin_service),
+):
+    """
+    Delete user
+    """
+    user = await admin_service.delete_user(user_id)
+    return BaseReturn(message="User deleted successfully", data=user)
+
+
+@router.post(
+    "/users/{user_id}/verify",
+    response_model=BaseReturn[UserSchema],
+    response_model_exclude_none=True,
+)
+async def change_verification(
+    user_id: str,
+    admin_service: AdminService = Depends(get_admin_service),
+):
+    """
+    Verify user
+    """
+    user = await admin_service.change_verify(user_id)
+    return BaseReturn(message="User verified successfully", data=user)
--- backend/app/users/schemas.py
+++ backend/app/users/schemas.py
@@ -1,6 +1,9 @@
-from typing import Optional
+from typing import Optional, List, Any
 
 from pydantic import BaseModel, ConfigDict, EmailStr, Field
+
+from app.auth.models import User
+from contextlib import contextmanager
 
 
 class UserUpdatePassword(BaseModel):
@@ -12,6 +15,13 @@ class UserUpdate(BaseModel):
     first_name: Optional[str] = None
     last_name: Optional[str] = None
     receive_notifications: bool | None = None
+
+
+class UserSchema(User):
+    hashed_password: Any = Field(exclude=True)
+    notifications_tasks_id: Any = Field(exclude=True)
+
+    model_config = ConfigDict(from_attributes=True)
 
 
 class StudentData(BaseModel):

85ba8550e35fc7da26df6b3015409aef8c0cb301
Denys Shevchenko <denshevchenko19@gmail.com>
2025-05-09 18:55:13 +0200

    Quick fix of collection deletion

--- backend/app/exam/models.py
+++ backend/app/exam/models.py
@@ -162,8 +162,7 @@ class Collection(Document, TimestampMixin):
         """Delete all questions linked to this collection when the collection is deleted"""
         question_ids = []
         for q in self.questions:
-            question_obj = await q.fetch()
-            question_ids.append(question_obj.id)
+            question_ids.append(q.ref.id)
         if question_ids:
             await Question.find({"_id": {"$in": question_ids}}).delete()
 
